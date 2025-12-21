815c82e97bb9e4ce49df52403a14b8e21ad1854c
kakeetopius <pius.kakeeto@strathmore.edu>
2025-12-21 19:39:49 +0300

    Changed shell script

--- testnet.sh
+++ testnet.sh
@@ -1,3 +1,18 @@
-for i in {1..10}; do
-docker run  -d --name netshoot-$i --network test nicolaka/netshoot sleep infinity
-done
+#!/usr/bin/env bash
+make() {
+    for i in {1..10}; do
+    docker run  -d --name netshoot-$i --network test nicolaka/netshoot sleep infinity
+    done
+}
+
+remove() {
+    docker rm -f $(docker ps -q --filter name=netshoot-)
+}
+
+if [ "$1" == "remove" ]; then
+    remove
+else
+    make
+fi
+
+
